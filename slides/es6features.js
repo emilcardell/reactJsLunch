4de616a153cff51d4a3c869e755fe63195407cc2
Emil Cardell <emil.cardell@gmail.com>
2015-10-25 16:55:25 +0100

    Added some more es6 examples

--- slides/es6features.js
+++ slides/es6features.js
@@ -1,6 +1,12 @@
-const aTextValue = {
+import evens from 'evens';
+
+const aStructure = {
     aFunction() {
-        let odds = evens.map(v => v + 1);
+        let odds = evens.map((v) => {
+            v + 1
+        });
         return odds;
     }
 };
+
+export default aStructure;
