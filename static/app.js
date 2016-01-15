e1f21450e6b3574081a701477688ecf5ecf5f550
Emil Cardell <emil.cardell@gmail.com>
2016-01-15 09:14:44 +0100

    Added non commited files

--- static/app.js
+++ static/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import SubApp from 'subApp'
 
 const App = React.createClass({
     render() {
-        return (<div>Application loaded!<SubApp /></div>);
+        return (<div>Hello world!! <SubApp message="Hello Sub World!" /></div>);
     }
 });
 
--- static/subApp.js
+++ static/subApp.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import NiceList from 'niceList';
 
 const SubApp = React.createClass({
+    getInitialState(){
+        return {
+            message: 'Hello from state!',
+            listOfNames: []
+        }
+    },
+    whenIPushTheButton(evt) {
+        let listOfNames = this.state.listOfNames;
+        listOfNames.push(this.state.message);
+
+        this.setState({
+            listOfNames: listOfNames
+        });
+    },
+    nameChanged(evt) {
+        let name = evt.target.value;
+
+
+        this.setState({
+            message: name
+
+        });
+    },
     render() {
-        return (<div>Sub Application loaded!</div>);
+
+
+
+        return (<div>
+            Sub Application loaded! {this.props.message} - {this.state.message}
+            <div>
+                <input type="text" ref="name" value={this.state.message} onChange={this.nameChanged}></input>
+                <button onClick={this.whenIPushTheButton}>Push me!</button>
+
+            </div>
+
+            <NiceList listOfNames={this.state.listOfNames}></NiceList>
+
+        </div>
+        );
     }
 });
 
